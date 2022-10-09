811c40765a8d418b74ea55ad1f3da5800e30a1cc
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-10-09 13:42:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector(".container input");
+const msg = document.querySelector("span.msg");
+const list = document.querySelector("ajax-section ul.cities");
+
+localStorage.setItem("tokenKeyEncrypted", EncryptStringAES);
